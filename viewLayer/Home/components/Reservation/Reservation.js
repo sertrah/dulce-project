0b6a7ede892502169dee62cabe77b9b3a8262cee
Harlen G <harlen.giraldo@mercadolibre.com>
2021-08-03 09:22:43 -0500

    added: new styles

--- viewLayer/Home/components/Reservation/Reservation.js
+++ viewLayer/Home/components/Reservation/Reservation.js
@@ -1,5 +1,6 @@
 import Button from "@material-ui/core/Button";
 
+import "./style.scss";
 
 import styles from "../../../../styles/Home.module.scss";
 
--- viewLayer/Home/components/Updates/Updates.js
+++ viewLayer/Home/components/Updates/Updates.js
@@ -1,11 +1,19 @@
 import styles from "../../../../styles/Home.module.scss";
 import Button from "@material-ui/core/Button";
 import Leaf from "./Leaf";
+import Flor from "../../../../public/img/Flor1.svg";
 
 const Updates = () => {
   return (
     <>
       <section className={styles.updates}>
+        <div className={styles.updatesFlor1}>
+          <Flor />
+          <Flor />
+         </div>
+         <div className={styles.updatesFlor2}>
+          <Flor />
+         </div>
         <Button variant="contained" className={styles.roundButton}>
           <div>
             <p>Eventos</p>
