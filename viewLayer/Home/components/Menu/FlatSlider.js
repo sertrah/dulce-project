55a348a88e763c1c7d3d013927ba4f7333990e88
Harlen G <harlen.giraldo@mercadolibre.com>
2021-08-29 11:24:34 -0500

    updated  menu component

--- viewLayer/Home/components/Menu/FlatSlider.js
+++ viewLayer/Home/components/Menu/FlatSlider.js
@@ -1,15 +1,52 @@
-import React, { useRef } from "react";
+import React, { useRef, useMemo, memo } from "react";
+import Button from "@material-ui/core/Button";
+
 import style from "../../../../styles/Home.module.scss";
 import { gsap } from "gsap";
+
+const Card = ({ title }) => {
+  return (
+    <div className={style.menu_card}>
+      <div className={style.menu_image}> </div>
+      <h2 className={style.menu_title}>
+        <span className={style.menu_title_subtitle}>Antojate </span>
+        {title}
+      </h2>
+      <Button size="large" variant="contained" className={style.menu_btn}>
+        probar
+      </Button>
+    </div>
+  );
+};
+const CardMemorized = memo(Card);
 
 const FlatSlider = ({ data }) => {
   const [active, setActive] = React.useState(0);
   const box1 = useRef(null);
   const slideDuration = 1;
-
+  const cardItems = useMemo(
+    () => [
+      {
+        title: "BRUNCH",
+        image:
+          "https://images.unsplash.com/photo-1514589868-e9e8d8b9e8a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+      },
+      {
+        title: "POSTRES",
+        image:
+          "https://images.unsplash.com/photo-1514589868-e9e8d8b9e8a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+      },
+      {
+        title: "TORTAS",
+        image:
+          "https://images.unsplash.com/photo-1514589868-e9e8d8b9e8a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+      }
+    ],
+    []
+  );
   const box2 = useRef(null);
   const box3 = useRef(null);
-  const [datt, setDatt] = React.useState(data);
+  const [datt, setDatt] = React.useState(cardItems);
   React.useEffect(() => {
     setInterval(() => {
       setDatt((prev) => [...prev.slice(1), prev[0]]);
@@ -45,13 +82,11 @@ const FlatSlider = ({ data }) => {
 
   return (
     <div className={style.slider}>
-      {datt.map((item, index) => {
-        return (
-          <div key={index} className={style.sliderItem} ref={setRef(index)}>
-            <img src={item.image} alt={item.title} style={{ width: 240 }} />
-          </div>
-        );
-      })}
+      {datt.map((item, index) => (
+        <div key={index} ref={setRef(index)} className={style.sliderItem}>
+          <CardMemorized key={`card-${index}`} {...item} />
+        </div>
+      ))}
     </div>
   );
 };
