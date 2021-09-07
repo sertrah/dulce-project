c957b0141e4fda43abe1dd1a3b92e3f028cc182d
G4by <08.gabiy@gmail.com>
2021-09-06 20:39:12 -0500

    se cambiaron las imagenes de la seccion del location

--- pages/index.js
+++ pages/index.js
@@ -67,19 +67,19 @@ const Home = () => {
       {
         key: 1,
         image: "https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?fit=crop&fm=jpg&h=825&q=80&w=1325",
-        title: "Slide #1",
+        title: "Antojate",
         caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       },
       {
         key: 2,
         image: "https://images.unsplash.com/photo-1445251836269-d158eaa028a6?fit=crop&fm=jpg&h=825&q=80&w=1325",
-        title: "Slide #2",
+        title: "Antojate",
         caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       },
       {
         key: 3,
         image: "https://images.unsplash.com/photo-1443926818681-717d074a57af?fit=crop&fm=jpg&h=825&q=80&w=1325",
-        title: "Slide #3",
+        title: "Antojate",
         caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       },
     ],
